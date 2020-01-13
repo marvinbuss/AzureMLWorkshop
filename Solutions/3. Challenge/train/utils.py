e867f6f71b13a1cc57fe253c40410c2d0a922eaa
Marvin Buss <marvin.buss@gmail.com>
2020-01-13 15:07:29 +0100

    updated paths and challenges

--- Solutions/3. Challenge/train/utils.py
+++ Solutions/3. Challenge/train/utils.py
@@ -4,6 +4,8 @@ import datetime
 import time
 import torch
 import torch.distributed as dist
+from torch import nn
+import torchvision
 
 import errno
 import os
@@ -230,6 +232,7 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
+    ''' This is setup by Azure ML
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
@@ -243,12 +246,123 @@ def init_distributed_mode(args):
         print('Not using distributed mode')
         args.distributed = False
         return
+    
+    #torch.cuda.set_device(args.gpu)
+    '''
+    args.distributed = args.world_size >= 2
+    
+    if args.distributed:
+        print('| distributed init (rank {}) (backend {}): {}'.format(args.rank, args.dist_backend, args.dist_url), flush=True)
+        torch.distributed.init_process_group(backend=args.dist_backend,
+                                            init_method=args.dist_url,
+                                            world_size=args.world_size,
+                                            rank=args.rank)
+        setup_for_distributed(args.rank <= 0)
 
-    args.distributed = True
 
-    torch.cuda.set_device(args.gpu)
-    print('| distributed init (rank {}) (backend {}): {}'.format(
-        args.rank, args.dist_backend, args.dist_url), flush=True)
-    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-                                         world_size=args.world_size, rank=args.rank)
-    setup_for_distributed(args.rank == 0)
+def initialize_model(num_classes, args):
+    print('Loading model')
+    model = torchvision.models.__dict__[args.model](pretrained=args.pretrained, progress=False)
+    
+    print('Change model settings')
+    if 'resnet' in args.model or 'resnext' in args.model:
+        ''' resnet18, resnet34, resnet50, resnet101, resnet152, resnext50_32x4d, resnext101_32x8d, wide_resnet50_2, wide_resnet101_2
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif 'alexnet' in args.model:
+        ''' alexnet
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif 'vgg' in args.model:
+        ''' vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn, vgg19, vgg19_bn
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif 'squeezenet' in args.model:
+        ''' squeezenet1_0, squeezenet1_1
+        '''
+        set_parameter_requires_grad(model, args)
+        model.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model.num_classes = num_classes
+        input_size = 224
+
+    elif 'densenet' in args.model:
+        ''' densenet121, densenet169, densenet201, densenet161
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.classifier.in_features
+        model.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif 'inception' in args.model:
+        ''' inception_v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        '''
+        set_parameter_requires_grad(model, args)
+        # Handle the auxilary net
+        num_ftrs = model.AuxLogits.fc.in_features
+        model.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+    
+    elif 'mobilenet' in args.model:
+        ''' mobilenet_v2
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.classifier[1].in_features
+        model.classifier[1] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    
+    elif 'shufflenet' in args.model:
+        ''' shufflenet_v2_x0_5, shufflenet_v2_x1_0, shufflenet_v2_x1_5, shufflenet_v2_x2_0
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    
+    elif 'mnasnet' in args.model:
+        ''' mnasnet0_5, mnasnet0_75, mnasnet1_0, mnasnet1_3
+        '''
+        set_parameter_requires_grad(model, args)
+        num_ftrs = model.classifier[1].in_features
+        model.classifier[1] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+    
+    # Gather the parameters to be optimized/updated in this run.
+    print('Get relevant model parameters')
+    params_to_update = get_params_to_update(model, args)
+
+    return model, input_size, params_to_update
+
+def set_parameter_requires_grad(model, args):
+    if args.finetuning:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def get_params_to_update(model, args):
+    params_to_update = []
+    if args.finetuning:
+        for param in model.parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+    else:
+        params_to_update = model.parameters()
+    return params_to_update
